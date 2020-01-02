fd185f9f21dbe02399aa8d0c7e398d5cef79beab
Duan Shiqiang <duanshiqiang1@gmail.com>
2020-01-02 21:14:32 +0800

    Update

--- config/shadowsocks/bin/ss-monitor.sh
+++ config/shadowsocks/bin/ss-monitor.sh
@@ -1,23 +1,18 @@
 #!/bin/sh
 LOGTIME=$(date "+%Y-%m-%d %H:%M:%S")
-LOGFILE=/var/log/shadowsocks_monitor.log
 COUNT=`wc -l $LOGFILE|awk '{print $1}'`
-#clear log if to many logs
-if [ $COUNT -gt 1000 ]; then
-        echo '' > $LOGFILE
-fi
 
 #check if shadowsocks ok
 curl --retry 1 --silent --connect-timeout 3 -I www.google.com > /dev/null
 if [ "$?" == "0" ]; then
-	echo '['$LOGTIME'] No Problem.' >> $LOGFILE
+	echo '['$LOGTIME'] No Problem.'
 	exit 0
 else
 	curl --retry 1 --silent --connect-timeout 3 -I www.baidu.com  > /dev/null
 	if [ "$?" == "0" ]; then
-		echo '['$LOGTIME'] Problem decteted, restarting shadowsocks.'  >> $LOGFILE
+		echo '['$LOGTIME'] Problem decteted, restarting shadowsocks.'
 		/etc/init.d/shadowsocks restart
 	else
-		echo '['$LOGTIME'] Network Problem. Do nothing.'  >> $LOGFILE
+		echo '['$LOGTIME'] Network Problem. Do nothing.'
 	fi
 fi
